bb6b3d32a8c2960ceecd717b835aab427146476b
fadhlanmr <fadhlan.mr@gmail.com>
2023-08-09 21:02:53 +0700

    tahukah kamu apa itu dimas?

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const { REST, Routes, Client, GatewayIntentBits } = require('discord.js');
+const { REST, Routes, Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const axios = require('axios');
-const {newManga} = require('./manga.js');
-const TOKEN = process.env.TOKEN
-const APP_ID = process.env.APP_ID
+const axios = require("axios");
+const { newManga } = require("./manga.js");
+const TOKEN = process.env.TOKEN;
+const APP_ID = process.env.APP_ID;
 
 // Exit handler
 // ["SIGTERM", "SIGINT"].forEach(event => {
@@ -14,43 +14,76 @@ const APP_ID = process.env.APP_ID
 //   });
 // });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'lucu') {
-    await interaction.reply('sePong!');
+  if (interaction.commandName === "lucu") {
+    await interaction.reply("sePong!");
   }
 
-  if (interaction.commandName === 'ping') {
+  if (interaction.commandName === "ping") {
     const chapter = 11;
-    resp = await axios.get('https://api.mangadex.org/manga/acdbf57f-bf54-41b4-8d92-b3f3d14c852e/');
-    manga = newManga(resp.data)
-    await interaction.reply({ content: `Manga ${manga.Title}, check out here ${manga.LatestChapter}` });
-  };
+    const resp = await axios.get(
+      "https://api.mangadex.org/manga/acdbf57f-bf54-41b4-8d92-b3f3d14c852e/"
+    );
+    manga = newManga(resp.data);
+    await interaction.reply({
+      content: `Manga ${manga.Title}, check out here ${manga.LatestChapter}`,
+    });
+  }
+
+  if (interaction.commandName === "crawler") {
+    const url_params = interaction.options.getString('url', true); // Required parameter
+    const class_params = interaction.options.getString('class_list');
+    await interaction.deferReply();
+    const resp = await axios.get("http://127.0.0.1:8000/parser", {
+      params: { url:url_params, class_list:class_params },
+    });
+    crawl_result = String(resp.data);
+    await interaction.editReply({ content: crawl_result });
+  }
 });
 
 const commands = [
   {
-    name: 'ping',
-    description: 'Replies with Pong!',
+    name: "ping",
+    description: "Replies with Pong!",
   },
   {
-    name: 'lucu',
-    description: 'Ketawa',
+    name: "lucu",
+    description: "Ketawa",
+  },
+  {
+    name: "crawler",
+    description: "kamu itu [isi sendiri]",
+    options: [
+      {
+        name: "url",
+        description: "URL for crawling",
+        required: true,
+        type: 3,
+      },
+      {
+        name: "class_list",
+        description: "tag class to crawl / get",
+        required: false,
+        type: 3,
+      },
+    ],
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
     await rest.put(Routes.applicationCommands(`${APP_ID}`), { body: commands });
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Successfully reloaded application (/) commands.");
 
     await client.login(TOKEN);
   } catch (error) {
