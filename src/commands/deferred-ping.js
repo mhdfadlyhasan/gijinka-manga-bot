080117bdba16d0570666f2c80ed8a59bbf50d53d
Arino Jenynof <arino.j17@gmail.com>
2024-04-30 14:04:02 +0700

    explain, explain, ...
    this shit so ass

--- src/commands/deferred-ping.js
+++ src/commands/deferred-ping.js
@@ -1,7 +1,7 @@
 import { InteractionResponseType } from "discord-interactions";
 import { json } from "itty-router";
 
-async function a231_deferred(interaction) {
+async function editReply(app_id, token) {
 	// Pura-pura GET
 	await new Promise(resolve => setTimeout(resolve, 5000));
 
@@ -11,7 +11,7 @@ async function a231_deferred(interaction) {
 	});
 	const body = JSON.stringify({ content: "sePong! ~5 seconds after.~" });
 	const req = new Request(
-		`https://discord.com/api/v10/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`,
+		`https://discord.com/api/v10/webhooks/${app_id}/${token}/messages/@original`,
 		{ method, headers, body }
 	);
 
@@ -22,9 +22,14 @@ export const deferred_ping = {
 	name: "deferred-ping",
 	description: "Replies with Pong! 5 seconds later",
 	execute({interaction, ctx}) {
-		ctx.waitUntil(a231_deferred(interaction));
+		// Ini kayak editReply, ctx.waitUntil() biar worker-nya gak langsung meninggoy
+		ctx.waitUntil(editReply(interaction.application_id, interaction.token));
+		
+		// Ini kayak deferReply
 		return json({
 			type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
 		});
+		
+		// Dibalik biar pusing
 	}
 };
