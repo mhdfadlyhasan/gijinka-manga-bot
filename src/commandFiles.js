d2d384e37b2822db992892fe50e76193df438659
Arino Jenynof <arino.j17@gmail.com>
2024-05-03 18:55:34 +0700

    Mulai nyoba port command yang ditinggal - belom kelar - ganti axios ke fetch api - embed (MessageComponent?) belum tau cara buildnya - ngab kalo ngodonf pake async/await jangan dicampur sama Promise.then() donf 😭

--- src/commandFiles.js
+++ src/commandFiles.js
@@ -1,2 +1,3 @@
 export { ping } from "./commands/ping.js";
 export { deferred_ping } from "./commands/deferred-ping.js";
+export { fourch } from "./commands/fourch.js";
---
+++ src/commands/fourch.js
@@ -0,0 +1,47 @@
+import { InteractionResponseType } from "discord-interactions";
+import { json } from "itty-router";
+
+async function _execute(app_id, token) {
+	// InteractionOptions di sini
+	const boardParams = "a";
+
+	let method = "GET";
+	let req = new Request(`https://a.4cdn.org/${boardParams}/catalog.json`, { method });
+	let res = await fetch(req);
+	const data = await res.json();
+
+	const catalogs = [];
+	for (const page of data) {
+		for (const item of page.threads) {
+			const catalogObj = {
+				thread: item.no,
+				title: item.sub,
+				body: item.com,
+				reply: item.replies,
+			};
+			catalogs.push(catalogObj);
+		}
+	}
+	catalogs.sort((a, b) => a.reply - b.reply);
+
+	// editReply
+	return await fetch(`https://discord.com/api/v10/webhooks/${app_id}/${token}/messages/@original`, {
+		method: "PATCH",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ content: catalogs[0].body })
+	});
+}
+
+export const fourch = {
+	name: "4cthread",
+	description: "do your lurk reps",
+	execute({ interaction, ctx }) {
+		ctx.waitUntil(_execute(interaction.application_id, interaction.token));
+
+		return json({
+			type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
+		});
+	}
+}
